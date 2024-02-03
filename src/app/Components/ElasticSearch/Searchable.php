8c4edaf298f9b89ede8ee0e09fb698c962cdafd5
andrey <andrey.develop.a@gmail.com>
2024-02-04 05:12:29 +0700

    feat: Searchable.php

--- src/app/Components/ElasticSearch/Searchable.php
+++ src/app/Components/ElasticSearch/Searchable.php
@@ -4,5 +4,20 @@ namespace App\Components\ElasticSearch;
 
 trait Searchable
 {
+    public static function bootSearchable()
+    {
+        if (config("services.search.enabled")) {
+            static::observe(ElasticsearchObserver::class);
+        }
+    }
 
+    public function getSearchIndex()
+    {
+        return $this->getTable();
+    }
+
+    public function toSearchArray()
+    {
+        return $this->toArray();
+    }
 }
--- src/app/Models/Article.php
+++ src/app/Models/Article.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Components\ElasticSearch\Searchable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     use HasFactory;
+    use Searchable;
 }
